0aa98371361d3d043f674c182f9ea1dcf1ab7121
absolutedimension <dk.peace@gmail.com>
2021-03-23 23:32:22 +0530

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+//import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import Sticky from 'react-stickynode';
+import { DrawerProvider } from '../containers/contexts/DrawerContext';
+import { saasModernTheme } from '../containers/theme/saasModern';
+import { ResetCSS } from '../containers/assets/css/style';
+import {
+  GlobalStyle,
+  ContentWrapper,
+} from '../containers/SaasModern/sassModern.style';
 
-function App() {
+import BannerSection from '../containers/SaasModern/Banner';
+import Navbar from '../containers/SaasModern/Navbar';
+import WorkingProcessSection from '../containers/SaasModern/WorkingProcess';
+import PricingSection from '../containers/SaasModern/Pricing';
+import PartnerSection from '../containers/SaasModern/Partner';
+import FaqSection from '../containers/SaasModern/Faq';
+import TrialSection from '../containers/SaasModern/Trial';
+import InfoSection from '../containers/SaasModern/Info';
+import FeatureSection from '../containers/SaasModern/Feature';
+import UpdateScreen from '../containers/SaasModern/UpdateScreen';
+import TestimonialSection from '../containers/SaasModern/Testimonial';
+import Footer from '../containers/SaasModern/Footer';
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ThemeProvider theme={saasModernTheme}>
+      <Fragment>
+   <header>
+          <title>SaaS | A react next landing page</title>
+          <meta name="Description" content="React next landing page" />
+          <meta name="theme-color" content="#ec5555" />
+          {/* Load google fonts */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,500i,700,900|Open+Sans:400,400i,600,700"
+            rel="stylesheet"
+          />
+       </header>
 
-export default App;
+        <ResetCSS />
+        <GlobalStyle />
+
+        <ContentWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          <BannerSection />
+          <WorkingProcessSection />
+          <InfoSection />
+          <FeatureSection />
+          <UpdateScreen />
+          <PricingSection />
+          <PartnerSection />
+          <TestimonialSection />
+          <FaqSection />
+          <TrialSection />
+          <Footer />
+        </ContentWrapper>
+      </Fragment>
+    </ThemeProvider>
+  );
+};
+
